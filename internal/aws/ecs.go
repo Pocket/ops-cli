5c11b022d5278742a9f108b4adb45556a519a531
Daniel Brooks <bass_rock@me.com>
2019-06-16 16:16:17 -0700

    fix(ecs): creating the ecs based client

--- internal/aws/ecs.go
+++ internal/aws/ecs.go
@@ -5,31 +5,44 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	"github.com/dnaeon/go-vcr/recorder"
+	"strings"
 )
 
-func DeployUpdate(clusterName *string, serviceName *string, imageName *string) {
-	//client, clientContext := ecsClient()
+type Client struct {
+	client        *ecs.Client
+	clientContext context.Context
+}
 
-	//TODO: get current service
-	//TODO: get current service task definition
-	//TODO: create a new task definition based on the current one and replace the image like here: https://github.com/fabfuel/ecs-deploy/blob/238a6715339f55b05cfe46338a8bc71d54aa4d93/ecs_deploy/ecs.py#L231
-	//TODO: deploy new task definition
+func New() *Client {
+	client, clientContext := ecsClient()
+	return &Client{
+		client:        client,
+		clientContext: clientContext,
+	}
+}
 
-	//client.DescribeServicesRequest(&ecs.DescribeServicesInput{
-	//	Cluster: clusterName,
-	//	Services: []string{
-	//		*serviceName,
-	//	},
-	//})
-	//
-	//client.DescribeTaskDefinitionRequest(&ecs.DescribeTaskDefinitionInput{
-	//
-	//})
-	//
-	//client.UpdateServiceRequest(&ecs.UpdateServiceInput{
-	//	Cluster:
-	//})
+func (c *Client) DeployUpdate(clusterName *string, serviceName *string, imageNames *[]string) {
 
+	deployment, err := c.getLatestDeployment(clusterName, serviceName)
+	if err != nil {
+		panic("error getting the latest deployment, " + err.Error())
+	}
+	taskDefinition, err := c.registerTaskDefinition(deployment.TaskDefinition, imageNames)
+
+	if err != nil {
+		panic("error registering the latest deployment, " + err.Error())
+	}
+
+	err = c.updateService(clusterName, serviceName, nil, &taskDefinition)
+
+	if err != nil {
+		panic("error updating the service, " + err.Error())
+	}
+
+	err = c.wait(clusterName, serviceName, &taskDefinition)
+	if err != nil {
+		panic("error waiting for the service, " + err.Error())
+	}
 }
 
 func ecsClient() (*ecs.Client, context.Context) {
@@ -45,11 +58,88 @@ func ecsClient() (*ecs.Client, context.Context) {
 }
 
 func awsECSRecorder() *recorder.Recorder {
-	r, err := recorder.New("_fixtures/cloudformation")
+	r, err := recorder.New("_fixtures/ecs")
 	if err != nil {
 		panic(err)
 	}
 	defer r.Stop() // Make sure recorder is stopped once done with it
 
 	return r
+}
+
+// RegisterTaskDefinition updates the existing task definition's image.
+func (c *Client) registerTaskDefinition(task *string, images *[]string) (string, error) {
+	output, err := c.client.DescribeTaskDefinitionRequest(&ecs.DescribeTaskDefinitionInput{
+		TaskDefinition: task,
+	}).Send(c.clientContext)
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, d := range output.TaskDefinition.ContainerDefinitions {
+		for _, image := range *images {
+			imageName := strings.SplitN(image, ":", 1)[0]
+			if strings.HasPrefix(*d.Image, imageName) {
+				d.Image = &image
+			}
+		}
+	}
+	input := &ecs.RegisterTaskDefinitionInput{
+		ContainerDefinitions: output.TaskDefinition.ContainerDefinitions,
+		Family:               output.TaskDefinition.Family,
+		NetworkMode:          output.TaskDefinition.NetworkMode,
+		PlacementConstraints: output.TaskDefinition.PlacementConstraints,
+		TaskRoleArn:          output.TaskDefinition.TaskRoleArn,
+		Volumes:              output.TaskDefinition.Volumes,
+	}
+	resp, err := c.client.RegisterTaskDefinitionRequest(input).Send(c.clientContext)
+	if err != nil {
+		return "", err
+	}
+	return *resp.TaskDefinition.TaskDefinitionArn, nil
+}
+
+// UpdateService updates the service to use the new task definition.
+func (c *Client) updateService(cluster, service *string, count *int64, arn *string) error {
+	input := &ecs.UpdateServiceInput{
+		Cluster: cluster,
+		Service: service,
+	}
+	if *count != -1 {
+		input.DesiredCount = count
+	}
+	if arn != nil {
+		input.TaskDefinition = arn
+	}
+	_, err := c.client.UpdateServiceRequest(input).Send(c.clientContext)
+	return err
+}
+
+// Wait waits for the service to finish being updated.
+func (c *Client) wait(cluster, service, arn *string) error {
+	return c.client.WaitUntilTasksRunning(c.clientContext, &ecs.DescribeTasksInput{
+		Tasks: []string{
+			*arn,
+		},
+	})
+}
+
+func (c *Client) getLatestDeployment(cluster, service *string) (*ecs.Deployment, error) {
+	input := &ecs.DescribeServicesInput{
+		Cluster:  cluster,
+		Services: []string{*service},
+	}
+	output, err := c.client.DescribeServicesRequest(input).Send(c.clientContext)
+	if err != nil {
+		return nil, err
+	}
+
+	ds := output.Services[0].Deployments
+	for _, d := range ds {
+		if *d.TaskDefinition == *output.Services[0].TaskDefinition {
+			return &d, nil
+		}
+	}
+	return nil, nil
 }
