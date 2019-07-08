b8c17d6b0b47c981fc719e301c541162a2c6eca2
Daniel Brooks <bass_rock@me.com>
2019-07-07 21:48:07 -0700

    fix(go-vcr): cleaning up go-vcr so it works

--- internal/aws/ecs/ecs.go
+++ internal/aws/ecs/ecs.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
-	"github.com/dnaeon/go-vcr/recorder"
 	"strings"
 )
 
@@ -54,18 +53,7 @@ func ecsClient() (*ecs.Client, context.Context) {
 	if err != nil {
 		panic("unable to load SDK config, " + err.Error())
 	}
-	cfg.HTTPClient.Transport = awsECSRecorder()
 	return ecs.New(cfg), context.Background()
-}
-
-func awsECSRecorder() *recorder.Recorder {
-	r, err := recorder.New("_fixtures/ecs")
-	if err != nil {
-		panic(err)
-	}
-	defer r.Stop() // Make sure recorder is stopped once done with it
-
-	return r
 }
 
 // RegisterTaskDefinition updates the existing task definition's image.
